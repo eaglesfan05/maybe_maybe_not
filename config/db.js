2c2bb5cd3c02fb0d347d3f43480062132f7b3ede
Khalil <khalilhall1221@gmail.com>
2020-04-25 19:41:45 -0400

    set up reqistration route with jwt

--- config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://khalil:Briana2804@cluster0-btwyy.mongodb.net/test?retryWrites=true&w=majority",
-};
--- server.js
+++ server.js
@@ -2,28 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
-
-const items = require("./routes/API/Items");
+const config = require("config");
 const app = express();
 
-//bodyparser middleware//
-app.use(bodyParser.json());
+//express middleware//
+app.use(express.json());
 
 //db config//
 // const db = require("./config/db").mongoURI;
 const db =
   process.env.NODE_ENV === "production"
     ? process.env.MongoURI
-    : require("./config/db").mongoURI;
+    : config.get("MongoURI");
 
 //connect to mongo//
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 //anything that goes to route refers to items variable
-app.use("/api/items", items);
+app.use("/api/items", require("./routes/API/Items"));
+app.use("/api/users", require("./routes/API/Users"));
 
 //server static assets(aka build folder) if in production
 if (process.env.NODE_ENV === "production") {
